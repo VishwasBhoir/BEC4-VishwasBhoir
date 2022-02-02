cdb2d943408c7d09b84566bd35debb2c48f2d92d
Vishwas Bhoir <CoderVishwas@gmail.com>
2022-02-02 23:45:37 +0530

    Day1-Assignment

---
+++ day1-Assignment1.js
@@ -0,0 +1,23 @@
+const add = (num1, num2) => {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+};
+
+const sub = (num1, num2) => {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+};
+
+const mul = (num1, num2) => {
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+};
+
+const div = (num1, num2) => {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+};
+
+let num1 = 30;
+let num2 = 10;
+
+add(num1, num2);
+sub(num1, num2);
+mul(num1, num2);
+div(num1, num2);
