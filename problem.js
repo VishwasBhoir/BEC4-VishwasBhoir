991b07860d7195a4610c7380d4dc70a6545ff39d
Vishwas Bhoir <72652750+VishwasBhoir@users.noreply.github.com>
2022-02-04 22:31:23 +0530

    .

--- problem.js
+++ problem.js
@@ -1,14 +1,37 @@
-let arr = [1,0,1,1,0,0,0];
-for (let index = arr.length-1; index > 0; index--) {
+let arr = [1, 0, 1, 1, 0, 0, 0];
+for (let index = arr.length - 1; index > 0; index--) {
     for (let i = 0; i < index; i++) {
-        if (arr[i]>arr[i+1]) {
+        if (arr[i] > arr[i + 1]) {
             let temp = arr[i];
             arr[i] = arr[i + 1];
             arr[i + 1] = temp;
-        };
+        }
+    }
+}
 
-    };
+console.log(arr);
+
+
+// counting the numbers
+
+let count0 = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+        count0++;
+    }
+
+}
+
+for (let i = 0; i < arr.length; i++) {
+    if (count0 > 0) {
+        arr[i] = 0;
+        count0--;
+    } else {
+        arr[i] = 1;
+    }
 
 }
 
 console.log(arr);
+
