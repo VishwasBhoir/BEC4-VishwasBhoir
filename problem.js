bbb786467ca09ba9ba4a7cc40a44b060c8037319
Vishwas Bhoir <72652750+VishwasBhoir@users.noreply.github.com>
2022-02-05 12:23:49 +0530

    Update problem.js

--- problem.js
+++ problem.js
@@ -35,3 +35,66 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(arr);
 
+
+
+// let arr = [1, 0, 1, 1, 0, 0, 0];
+// for (let index = arr.length - 1; index > 0; index--) {
+//     for (let i = 0; i < index; i++) {
+//         if (arr[i] > arr[i + 1]) {
+//             let temp = arr[i];
+//             arr[i] = arr[i + 1];
+//             arr[i + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log(arr);
+
+// // counting the numbers
+
+// let count0 = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 0) {
+//         count0++;
+//     }
+
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (count0 > 0) {
+//         arr[i] = 0;
+//         count0--;
+//     } else {
+//         arr[i] = 1;
+//     }
+
+// }
+
+// console.log(arr);
+
+// let n = 3;
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+//     for (let j = 1; j <= i; j++) {
+//         if (i == j) {
+//             continue;
+//         }
+//         console.log(i, j);
+
+//     }
+// }
+
+
+let num = 4;
+
+switch (true) {
+    case (num>0):
+        console.log("positive");
+        break;
+    case (num < 0):
+        console.log("Negative");
+        break;
+    default:
+        console.log("zero");
+}
